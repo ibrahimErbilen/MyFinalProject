e87a4e2394aadcc1e99c5734fc9e4a7a0e680737
İbrahim ERBİLEN <ibrahimerblnn46@gmail.com>
2024-02-15 22:03:54 +0300

    Add project files.

---
+++ Business/Concreate/ProductManager.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concreate.InMemory;
+using Entities.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concreate
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //İş kodları
+            //Yetkisi var mı?
+
+            return _productDal.GetAll();
+
+
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using Business.Concreate;
+using DataAccess.Concreate.InMemory;
+using Entities.Concreate;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        { 
+            ProductManager  productManager = new ProductManager(new InMemoryProductDal());
+
+            foreach (var product in productManager.GetAll())
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
+
+        }
+    }
+}
